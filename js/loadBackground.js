7de0f6a7e1d417763888031bf1013295288f700b
funinfofiles <38995682+funinfofiles@users.noreply.github.com>
2020-06-26 19:33:15 +0800

    fix: typo

--- js/loadBackground.js
+++ js/loadBackground.js
@@ -3,9 +3,11 @@
  * ISAB Dev Team
  * @description
  * Awesome background images for ISAB pages.
- * You can Load this file from jsDeilvr:
+ * The latest version can always be found here:
+ * https://github.com/urlib/static.isab.top/blob/master/js/loadBackground.js
+ * You can Load this file from jsDelivr:
  * https://cdn.jsdelivr.net/gh/urlib/static.isab.top@latest/js/loadBackground.js
- * There is also a minified version:
+ * There is also a minified version (thanks to jsDelivr):
  * https://cdn.jsdelivr.net/gh/urlib/static.isab.top@latest/js/loadBackground.min.js
  */
 
@@ -14,12 +16,12 @@
     const backgroundImageList = [
         {
             name: 'Galaxy',
-            png: 'gh/urlib/bin_0@f5a296f9/fe/94/83/5f/fe94835f3f8985e5965b9fe974d1917858affba03182c2aef5071ffbfcaee61c.png',
+            origin: 'gh/urlib/bin_0@f5a296f9/fe/94/83/5f/fe94835f3f8985e5965b9fe974d1917858affba03182c2aef5071ffbfcaee61c.png',
             webp: 'gh/urlib/bin_0@f5a296f9/95/11/c2/d9/9511c2d95bdce8999a519c9f7c1910b316c4c6728bf6b7ef7ee9cdfd1a0faeb1.webp'
         },
         {
             name: 'Purple Galaxy',
-            png: 'gh/urlib/bin_0@f5a296f9/db/2e/1e/54/db2e1e54c1152b4dfef159d9668427876d7f5cd0a1a8e8caa83afa83cb4914de.png',
+            origin: 'gh/urlib/bin_0@f5a296f9/db/2e/1e/54/db2e1e54c1152b4dfef159d9668427876d7f5cd0a1a8e8caa83afa83cb4914de.png',
             webp: 'gh/urlib/bin_0@f5a296f9/fa/8b/c2/54/fa8bc254ad36808b3bf8d110e11678acb4206c07f12f95ff5d23d8920268259d.webp'
         }
     ];
@@ -37,7 +39,7 @@
     };
     const imageChoice = (isWebpSupported) => {
         const image = randomChoice(backgroundImageList);
-        return completeUrl(isWebpSupported ? image.webp : image.png);
+        return completeUrl(isWebpSupported ? image.webp : image.origin);
     };
     const imageUrl = imageChoice(isWebpSupported);
 
