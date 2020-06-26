8e14c01ce9949ae4ac1d9990402cc022bbdb8950
Joe Fang <joefang20031128@hotmail.com>
2020-06-26 20:29:14 +0800

    feature: new images && fix

--- js/loadBackground.js
+++ js/loadBackground.js
@@ -1,12 +1,16 @@
 /**
  * @author
  * ISAB Dev Team
+ * 
  * @description
  * Awesome background images for ISAB pages.
+ * 
  * The latest version can always be found here:
  * https://github.com/urlib/static.isab.top/blob/master/js/loadBackground.js
- * You can Load this file from jsDelivr:
+ * 
+ * You can Load this file from jsDelivr (faster and more reliable):
  * https://cdn.jsdelivr.net/gh/urlib/static.isab.top@latest/js/loadBackground.js
+ * 
  * There is also a minified version (thanks to jsDelivr):
  * https://cdn.jsdelivr.net/gh/urlib/static.isab.top@latest/js/loadBackground.min.js
  */
@@ -23,6 +27,16 @@
             name: 'Purple Galaxy',
             origin: 'gh/urlib/bin_0@f5a296f9/db/2e/1e/54/db2e1e54c1152b4dfef159d9668427876d7f5cd0a1a8e8caa83afa83cb4914de.png',
             webp: 'gh/urlib/bin_0@f5a296f9/fa/8b/c2/54/fa8bc254ad36808b3bf8d110e11678acb4206c07f12f95ff5d23d8920268259d.webp'
+        },
+        {
+            name: 'milky-way-2695569',
+            origin: 'gh/urlib/bin_0@9df0c1b8/4e/4a/cc/27/4e4acc27fe5141b1569159fd67fd0a3f75bd49d473db7cb037cc34b26ab9345b.jpg',
+            webp: 'gh/urlib/bin_0@9df0c1b8/04/63/7c/ad/04637cadfd2f562927ba40237a4dddf8e4e777a40332fab19f1ab6ef84408fa1.webp'
+        },
+        {
+            name: 'ocean-3605547',
+            origin: 'gh/urlib/bin_0@9df0c1b8/6a/81/d7/86/6a81d7861a3edeb015f3dd815a1af17f739ac25e6e0cddecb98f818c6de1e030.jpg',
+            webp: 'gh/urlib/bin_0@9df0c1b8/09/61/ed/d9/0961edd9edc1a2b0c98fdc6e9254219e1bd4af77eeef8a1f5ee9baa89503730b.webp'
         }
     ];
 
@@ -44,6 +58,9 @@
     const imageUrl = imageChoice(isWebpSupported);
 
     window.addEventListener('load', () => {
-        document.body.style.backgroundImage = `url(${imageUrl})`;
+        const style = document.body.style;
+        style.backgroundImage = `url(${imageUrl})`;
+        style.backgroundAttachment = 'fixed';
+        style.backgroundSize = '100% 100%';
     });
 })();
